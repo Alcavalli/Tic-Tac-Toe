680f491fa24f028279a7cbd7d548ad959589c1e5
Alcavalli <alcavalli09@gmail.com>
2026-06-08 19:20:26 +0200

    third commit, first try: headers Constants and Enums

---
+++ include/Constants.hpp
@@ -0,0 +1,11 @@
+namespace Constants
+{
+    //* Griglia
+    constexpr int ROWS = 3, COLS = 3;
+
+    //* Celle
+    constexpr int CELL_SIZE = 150;
+
+    //* Window
+    constexpr int WINDOW_HEIGHT = CELL_SIZE * ROWS, WINDOW_WIDTH = CELL_SIZE * COLS;
+}
---
+++ include/Enums.hpp
@@ -0,0 +1,11 @@
+enum class StatoGioco
+{
+    InProgress,
+    GameOver
+};
+
+enum class Winner
+{
+    Player1,
+    Player2
+};
