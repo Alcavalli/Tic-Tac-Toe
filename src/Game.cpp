9e826b6144e381ec35f36dbf3f47444c0f7cdc5a
Alcavalli <alcavalli09@gmail.com>
2026-06-09 16:18:23 +0200

    eleventh commit, fourth attempt: completed the logic part, render and AI still missing

--- src/Game.cpp
+++ src/Game.cpp
@@ -23,31 +23,35 @@ void Game::processInput()
 
         if (const auto *mouseClick{event->getIf<sf::Event::MouseButtonPressed>()})
             if (mouseClick->button == sf::Mouse::Button::Left)
-                update(mouseClick->position.y / Constants::CELL_SIZE, mouseClick->position.x / Constants::CELL_SIZE);       //! Ricorda: riga e colonna, quindi y e x (non x e y)
+                update(mouseClick->position.y / Constants::CELL_SIZE, mouseClick->position.x / Constants::CELL_SIZE); //! Ricorda: riga e colonna, quindi y e x (non x e y)
+
+        if (const auto *keyPress{event->getIf<sf::Event::KeyPressed>()})
+            if (stato_gioco != GameStatus::InProgress)
+                reset();
     }
 }
 
 void Game::update(int row, int col)
 {
-    if (table.getCell(row, col) != CellStatus::Empty)
-        throw std::runtime_error("ERROR: the cell is already occupied");
+    if (table.getCell(row, col) != CellStatus::Empty || stato_gioco != GameStatus::InProgress)
+        return;
     else
     {
         table.setCell(row, col, turno_corrente);
-        switch(table.checkWinner())
+        switch (table.checkWinner())
         {
-            case CellStatus::Empty:
-                if (table.isFull())
-                    stato_gioco = GameStatus::Draw;
-                else
-                    turno_corrente = (turno_corrente == CellStatus::Player1) ? CellStatus::Player2 : CellStatus::Player1;
-                break;
-            case CellStatus::Player1:
-                stato_gioco = GameStatus::Player1Win;
-                break;
-            case CellStatus::Player2:
-                stato_gioco = GameStatus::Player2Win;
-                break;
+        case CellStatus::Empty:
+            if (table.isFull())
+                stato_gioco = GameStatus::Draw;
+            else
+                turno_corrente = (turno_corrente == CellStatus::Player1) ? CellStatus::Player2 : CellStatus::Player1;
+            break;
+        case CellStatus::Player1:
+            stato_gioco = GameStatus::Player1Win;
+            break;
+        case CellStatus::Player2:
+            stato_gioco = GameStatus::Player2Win;
+            break;
         }
     }
 }
